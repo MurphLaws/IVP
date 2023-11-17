3e0584fdd9169e048045540fcd9070dc4e0ff833
Nicolas Lasso <lasso.nicolas@correounivalle.edu.co>
2023-11-17 17:35:48 +0100

    Need to rotate box first and then all back, need to apply first box as well for testing

--- TP3-2.py
+++ TP3-2.py
@@ -3,21 +3,21 @@ import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import tkinter as tk
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw
 import random
 import bisect
 import imageio
 import argparse
 import numpy as np
 import copy
-
+import kornia
 
 class Box:
     def __init__(self, p1, p2,p3,p4,scale, angle):
-        self.p1 = p1
-        self.p2 = p2
-        self.p3 = p3
-        self.p4 = p4
+        self.p1 = int(p1[0]), int(p1[1])
+        self.p2 = int(p2[0]), int(p2[1])
+        self.p3 = int(p3[0]), int(p3[1])
+        self.p4 = int(p4[0]), int(p4[1])
         self.scale = scale
         self.angle = angle
 
@@ -46,6 +46,38 @@ class Box:
         )
 
         return self
+    
+    def rotate_box(self):
+        angle_rad = np.radians(self.angle)
+
+        # Calculate the center of the bounding box
+        center_x = (self.p1[0] + self.p2[0] + self.p3[0] + self.p4[0]) / 4
+        center_y = (self.p1[1] + self.p2[1] + self.p3[1] + self.p4[1]) / 4
+
+        # Translate the bounding box points so that the center is at the origin
+        translated_points = np.array([
+            [self.p1[0] - center_x, self.p1[1] - center_y],
+            [self.p2[0] - center_x, self.p2[1] - center_y],
+            [self.p3[0] - center_x, self.p3[1] - center_y],
+            [self.p4[0] - center_x, self.p4[1] - center_y]
+        ])
+
+        # Define the rotation matrix
+        rotation_matrix = np.array([[np.cos(angle_rad), -np.sin(angle_rad)],
+                                    [np.sin(angle_rad), np.cos(angle_rad)]])
+
+        # Rotate the translated bounding box points
+        rotated_points = np.dot(rotation_matrix, translated_points.T).T
+
+        # Translate the points back to their original position
+        rotated_points += np.array([center_x, center_y])
+
+        # Extract rotated points and assign them to the box as integer values
+        self.p1 = (int(rotated_points[0][0]), int(rotated_points[0][1]))
+        self.p2 = (int(rotated_points[1][0]), int(rotated_points[1][1]))
+        self.p3 = (int(rotated_points[2][0]), int(rotated_points[2][1]))
+        self.p4 = (int(rotated_points[3][0]), int(rotated_points[3][1]))
+
 
 
     def applyRotation(self, angle):
@@ -85,21 +117,19 @@ class Box:
 
         return self
 
-    def getBoundingBox(self):
-        points = [self.p1, self.p2,self.p3,self.p4]
+
+
+    
+    def getImagePortion(self, image):
+
+        points = [self.p1, self.p2, self.p3, self.p4]
 
         min_x = min(points, key=lambda t: t[0])[0]
         max_x = max(points, key=lambda t: t[0])[0]
         min_y = min(points, key=lambda t: t[1])[1]
         max_y = max(points, key=lambda t: t[1])[1]
 
-        return (min_x, min_y), (max_x, max_y)
-    
-    def getImagePortion(self, image):
-
-        #Make and empty image using black pixels with the size of bounding box
-
-        box = self.getBoundingBox()
+        box = ((min_x, min_y), (max_x, max_y))
         x1, y1 = box[0]
         x2, y2 = box[1]
 
@@ -107,10 +137,39 @@ class Box:
 
     def get_vertices(self):
         return [list(self.p1), list(self.p2), list(self.p3), list(self.p4)]    
+    
+    def crop_image(self, image, bbox):
+        # Calculate the bounding box coordinates
+        left, top = np.min(bbox, axis=0).astype(int)
+        right, bottom = np.max(bbox, axis=0).astype(int)
 
+        # Crop the image
+        cropped_image = image[top:bottom, left:right, :]
+
+        return cropped_image
+    
+    @staticmethod
+    def rotate_image(img_array, particle):
+
+        angle = particle[3]
+        pivot = particle[0], particle[1]
+
+    
+        # Calculate the translation to bring the pivot to the center
+        translate_x = img_array.shape[1] // 2 - int(pivot[0])
+        translate_y = img_array.shape[0] // 2 - int(pivot[1])
+
+        # Apply the translation
+        translated_image = np.roll(img_array, (translate_y, translate_x), axis=(0, 1))
+
+        # Apply the rotation using a geometric transformation
+        rotated_image = np.array(Image.fromarray(translated_image).rotate(angle, resample=Image.BICUBIC))
+
+        return rotated_image
 
 
 class ParticleFilter:
+
 
     def __init__(self, path, box):
         self.path = path
@@ -178,9 +237,8 @@ class ParticleFilter:
         
             x += np.random.normal(0, variance)
             y += np.random.normal(0, variance)
-            scale = p[2] + np.random.normal(0, 0.001)
-
-            angle = p[3] + np.random.normal(0, 0.001)
+            scale = p[2] + np.random.normal(0, 0.01)
+            angle = p[3] + np.random.normal(0, 0.01)
             new_particles.append((int(x), int(y),scale,angle, 1/len(particles)))
 
         return new_particles
@@ -194,6 +252,8 @@ class ParticleFilter:
         return distances
    
 
+
+    
     @staticmethod
     def systematic_resampling(particles):
         N = len(particles)
@@ -226,6 +286,7 @@ class ParticleFilter:
         updated_particles = [(p[0], p[1],p[2],p[3],p[4]/sum_weights) for p in updated_particles]
         return updated_particles
 
+
     def run_particle_filter(self):
                 frames = list(self.get_frames())
                 particles = self.initialize_particles(2, 5)
@@ -245,37 +306,40 @@ class ParticleFilter:
                     particles = self.prediction_step(particles, 5)
                  
                     distances = self.histogram_distance(first_hist, self.get_histograms(particles, frames[i]))
+
+                    #New distances should be an array of the same size as disances, but radom nuibers hat add up to 1
+
+                    new_distances = np.random.dirichlet(np.ones(len(distances)), size=1)[0]
            
-                    particles = self.update_particles(particles, distances)
+                    particles = self.update_particles(particles, new_distances)
 
                     particles = self.systematic_resampling(particles)
-                
-
             
                     x_mean = np.sum([p[0]*p[4] for p in particles])
                     y_mean = np.sum([p[1]*p[4] for p in particles])
                     s_mean = np.sum([p[2]*p[4] for p in particles])
                     a_mean = np.sum([p[3]*p[4] for p in particles])
-                  
-                
-
 
                     best_particle = (int(x_mean), int(y_mean), s_mean, a_mean)
 
-                    
                     asd.append(particles)
                   
                     best_particles.append(best_particle)
 
                     boxa = Box((best_particle[0]-box_size, best_particle[1]-box_size), (best_particle[0]+box_size, best_particle[1]-box_size), (best_particle[0]+box_size, best_particle[1]+box_size), (best_particle[0]-box_size, best_particle[1]+box_size),best_particle[2],best_particle[3])
                     boxy = boxa.applyScaling(s_mean).applyRotation(a_mean)
+                
                     sdf.append(boxy.get_vertices())
                     
                 return asd ,sdf, best_particles
+    
+
+
+
 
 
 center_x, center_y = 640/2, 480/2
-box_size= 25
+box_size= 20
 pf = ParticleFilter("escrime-4-3.avi", Box((center_x-box_size, center_y-box_size), (center_x+box_size, center_y-box_size), (center_x+box_size, center_y+box_size), (center_x-box_size, center_y+box_size),1,0))
 particles,boxes,bp = pf.run_particle_filter()
 frames = list(pf.get_frames())
@@ -308,11 +372,18 @@ def main(save_gif=False):
 
 
 first_frame = frames[0]
-boxo = Box((center_x-box_size, center_y-box_size), (center_x+box_size, center_y-box_size), (center_x+box_size, center_y+box_size), (center_x-box_size, center_y+box_size),1,0)
-plt.figure(figsize=(10,4))
-#Plot boxo 
-plt.plot([boxo.p1[0], boxo.p2[0], boxo.p3[0], boxo.p4[0], boxo.p1[0]], [boxo.p1[1], boxo.p2[1], boxo.p3[1], boxo.p4[1], boxo.p1[1]], label='boxo')
-boxo = boxo.applyRotation(20)
-plt.imshow(boxo.getImagePortion(first_frame))
-plt.show()
+boxo = Box((center_x-box_size, center_y-box_size), (center_x+box_size, center_y-box_size), (center_x+box_size, center_y+box_size), (center_x-box_size, center_y+box_size),1,20)
+#plt.figure(figsize=(10,4))
+#boxo.rotate_box()
+#plt.imshow(boxo.getImagePortion(first_frame))
+#plt.show()
 
+
+particle = (center_x, center_y, 1, 30)
+
+rot_img = boxo.rotate_image(first_frame, particle)
+bound_rot_image = boxo.getImagePortion(rot_img)
+
+plt.figure(figsize=(10,4))
+plt.imshow(bound_rot_image)
+plt.show()
